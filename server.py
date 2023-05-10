fd3878ef79d2cfc03a4ab6bdaa813953848d373b
dmitriyl99 <dmitriyl1899@gmail.com>
2023-05-10 16:12:38 +0500

    Add origin

--- server.py
+++ server.py
@@ -103,7 +103,7 @@ goods = [
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ruslanevent.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
